eaee10bc21eef1137efd987498025f75cce63a38
sjdkian <78721995+sjdkian@users.noreply.github.com>
2021-09-16 13:39:46 +0430

    add homepage

---
+++ src/documents/misc/homePage.md
@@ -0,0 +1,14 @@
+---
+title: homePage
+slug: homePage
+---
+
+## ترجمه آزاد کتاب Django for Professionals
+
+شما میتوانی از 
+[اینجا](/book)
+شروع به خواندن کنید.
+
+یابرای آشنایی به صفحه 
+[درباره](/about)
+مراجعه کنید.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import 'bootstrap/dist/css/bootstrap.css';
 
 // markup
-export default function Home( ) {
+export default function Home( {data} ) {
   
+  const {html} = data.homePage.nodes[0]
+
   
   return (
     <Layout>
         <div className="container" id="page-content-wrapper">
-          
-          <h1>خوش آمدید</h1>
-          <p>ترجمه آزاد کتاب Django for Professionals</p>
-          <p>شما می توانید از<Link to={"/book"} className="nav-link">اینجا</Link>شروع به خواندن کتاب کنید</p>
+          <div dangerouslySetInnerHTML={{ __html: html}} />
          </div>
         
 
     </Layout>
   )
 }
+
+export const query = graphql`
+query homeQuery {
+  
+  homePage: allMarkdownRemark(filter: {frontmatter: {slug: {eq: "homePage"}}}) {
+    nodes {
+      html
+    }
+  }
+}
+
+`
